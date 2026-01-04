3abfffec3823991fcf7e2e74def9e1313db0b0c2
Albert Sodel <sodelalbert@gmail.com>
2026-01-04 09:39:14 +0100

    Add HomePage and RegisterPage classes, update README and .env.sample

--- base/base.test.ts
+++ base/base.test.ts
@@ -1,6 +1,6 @@
 import { test as base } from "@playwright/test";
-import { HomePage } from "../pages/home.page";
-import { RegisterPage } from "../pages/register.page";
+import { HomePage } from "../pages/home-page";
+import { RegisterPage } from "../pages/register-page";
 import { UserGenerator } from "../utils/user-generator";
 
 type myFixtures = {
